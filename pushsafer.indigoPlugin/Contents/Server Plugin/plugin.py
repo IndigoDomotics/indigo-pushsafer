90ca2cb4254d8d598ca31391003ea8b4803a5e2b
Joe Keenan <joe@flyingdiver.com>
2018-03-25 12:22:45 -0400

    Working file attachments
    Cleaned up Action dialog.
    Still need error checking for max size.

--- pushsafer.indigoPlugin/Contents/Server Plugin/plugin.py
+++ pushsafer.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -1,14 +1,16 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os, httplib, urllib
+import os
 import requests
+import base64
 
 class Plugin(indigo.PluginBase):
 
     def __init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs):
         indigo.PluginBase.__init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs)
         self.debug = True
+        self.fileParameters = ["p", "p2", "p3"]
 
     def __del__(self):
         indigo.PluginBase.__del__(self)
@@ -49,7 +51,7 @@ class Plugin(indigo.PluginBase):
         }
 
         #populate optional parameters
-        
+                
         if self.present(pluginAction.props.get('msgTitle')):
             params['t'] = self.prepareTextValue(pluginAction.props['msgTitle'])
 
@@ -65,28 +67,55 @@ class Plugin(indigo.PluginBase):
         if self.present(pluginAction.props.get('msgIcon')):
             params['i'] = pluginAction.props["msgIcon"].strip()         
             
-        if self.present(pluginAction.props.get('msgTime2Live')):
-            params['l'] = pluginAction.props["msgTime2Live"].strip()            
-
+        if self.present(pluginAction.props.get('msgSupLinkUrl')):
+            params['u'] = self.prepareTextValue(pluginAction.props['msgSupLinkUrl'])
+       
         if self.present(pluginAction.props.get('msgSupLinkTitle')):
             params['ut'] = self.prepareTextValue(pluginAction.props['msgSupLinkTitle'])
 
-        if self.present(pluginAction.props.get('msgSupLinkUrl')):
-            params['u'] = self.prepareTextValue(pluginAction.props['msgSupLinkUrl'])
+        if self.present(pluginAction.props.get('msgTime2Live')):
+            params['l'] = pluginAction.props["msgTime2Live"].strip()            
 
-        self.debugLog(u"Params: {}".format(params))
+        if self.present(pluginAction.props.get('msgAttachment')):
+            count = 0
+            files = self.prepareTextValue(pluginAction.props['msgAttachment'])
+            fileList = files.split(",")
+            for file in fileList:
 
-#        conn = httplib.HTTPSConnection("pushsafer.com:443")
-#        conn.request(
-#           "POST",
-#           "/api",
-#           "/data/push-send.php",
-#           urllib.urlencode(params),
-#           {"Content-type": "application/x-www-form-urlencoded"}
-#        )
-#        r = conn.getresponse().read()
-#        conn.close()
-#        self.debugLog(u"Result: {}".format(r))
-       
-        r = requests.post("https://pushsafer.com/api", data = params)
+                if count == len(self.fileParameters):
+                    self.debugLog(u"Warning: too many files specified, skipping the rest...")
+                    break
+
+                attachFile = file.strip().lower()
+
+                if os.path.isfile(attachFile) and attachFile.endswith(('.jpg', '.jpeg')):
+                    if os.path.getsize(attachFile) <= 2621440:
+                        with open(attachFile, "rb") as image_file:
+                            params[self.fileParameters[count]] = "data:image/jpeg;base64," + base64.b64encode(image_file.read())
+                            self.debugLog(u"Including as parameter '{}': {}".format(self.fileParameters[count], attachFile))
+                    else:
+                        self.debugLog(u"Warning: attached file '{}' was too large, attachment was skipped".format(attachFile))
+                    
+                elif os.path.isfile(attachFile) and attachFile.endswith('.png'):
+                    if os.path.getsize(attachFile) <= 2621440:
+                        with open(attachFile, "rb") as image_file:
+                            params[self.fileParameters[count]] = "data:image/png;base64," + base64.b64encode(image_file.read())
+                            self.debugLog(u"Including as parameter '{}': {}".format(self.fileParameters[count], attachFile))
+                    else:
+                        self.debugLog(u"Warning: attached file '{}' was too large, attachment was skipped".format(attachFile))
+                    
+                elif os.path.isfile(attachFile) and attachFile.endswith('.gif'):
+                    if os.path.getsize(attachFile) <= 2621440:
+                        with open(attachFile, "rb") as image_file:
+                            params[self.fileParameters[count]] = "data:image/gif;base64," + base64.b64encode(image_file.read())
+                            self.debugLog(u"Including as parameter '{}': {}".format(self.fileParameters[count], attachFile))
+                    else:
+                        self.debugLog(u"Warning: attached file '{}' was too large, attachment was skipped".format(attachFile))
+                        
+                else:
+                    self.debugLog(u"Warning: file '{}' does not exist, or is not a supported file type, attachment was skipped".format(attachFile))
+                
+                count = count + 1
+
+        r = requests.post("https://www.pushsafer.com/api", data = params)
         self.debugLog(u"Result: {}: {}".format(r.status_code, r.text))
